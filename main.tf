1df221b4d3200a1c8f3683c80cdab387c69bddad
pramodpawade <sayali.punse@hotmail.com>
2022-01-17 09:49:38 +0000

    added backend s3 and dynamodb

--- main.tf
+++ main.tf
@@ -4,22 +4,36 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "b2000000000pilla" {
-  bucket = "pillas3"
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "pillas3-terraform-state"
   acl    = "private"
 
 
 }
 
+terraform {
+
+  backend "s3" {
+    //access_key = var.access_key
+    //secret_key = var.secret_key
+    bucket     = "pillas3-terraform-state"
+    key        = "pillas3/terraform.tfstate"
+    region     = "us-west-2"
+    dynamodb_table = "cpillas3-dynamodb-lock-state"
+  }
+  
+}
+
+
 resource "aws_dynamodb_table" "client_data" {
-  name = "client_csv_file"
+  name = "cpillas3-dynamodb-lock-state"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
-  hash_key       = "company_name"
+  hash_key       = "LockID"
 
   attribute {
-    name = "company_name"
+    name = "LockID"
     type = "S"
   }
 }
